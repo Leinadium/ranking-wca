378895ff3f4bfdca3bb8086598f541d962b43a38
Leinadium <daniel.sch.guima@gmail.com>
2024-09-23 07:40:37 -0300

    WIP: api start

---
+++ api/go.mod
@@ -0,0 +1,3 @@
+module github.com/Leinadium/ranking-wca-api
+
+go 1.22.2
--- db/schema/app.sql
+++ db/schema/app.sql
@@ -1,10 +1,4 @@
-CREATE DATABASE app;
-
-CREATE TABLE IF NOT EXISTS app.states (
-    state_id            CHAR(2) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL,
-    state_name          VARCHAR(20) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL,
-    PRIMARY KEY (state_id)
-);
+ 
 
 CREATE TABLE IF NOT EXISTS app.registered_users (
     wca_id              VARCHAR(10) NOT NULL,
